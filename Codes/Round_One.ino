07cb0686e84ee94ea0a4890a97c898a1d3005fbe
ERA-FutureEngineers <aliyevemil2445@gmail.com>
2024-10-16 14:35:31 +0400

    Update Round_One.ino

--- Codes/Round_One.ino
+++ Codes/Round_One.ino
@@ -1,10 +1,6 @@
 #include <Servo.h>
 
-
-int frontdist = 0;
-int leftdist = 0;
-int rightdist = 0;
-int Kp = 1.5, Kd = 0.4, errold = 0, err = 0, U = 0, target = 30, L = 0, R = 0, c = 0;
+int Kp = 1.5, Kd = 0.4, errold = 0, err = 0, U = 0, targetR = 25, targetL = 25, target = 45, L = 0, R = 0;
 
 Servo servo_9;
 
@@ -22,6 +18,19 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
   return pulseIn(echoPin, HIGH);
 }
 
+#define S0 2
+#define S1 10
+#define S2 12
+#define S3 11
+#define sensorOut 13
+
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+int k = 0, r = 0, oldr = 0;
+int previousMillis = 0;
+int interval = 4000;
+
 
 void setup()
 {
@@ -37,146 +46,135 @@ void setup()
   delay(1500);
 
   
-    analogWrite(6, 75);
-    analogWrite(5, 80);
-    digitalWrite(8, HIGH);
+    analogWrite(6, 84);
+    analogWrite(5, 77);
+    digitalWrite(7, HIGH);
     digitalWrite(4, HIGH);
-    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
     digitalWrite(3, LOW);  
-
 }
 
 void loop()
 {
-    //Desicion
-    if(L == 0 and R == 0)
-  {
-  leftdist = 0.01723 * readUltrasonicDistance(12, 13);
-  rightdist = 0.01723 * readUltrasonicDistance(11, 10);
 
+if(L == 0 and R == 0)
+  {
+  float leftdist = 0.01723 * readUltrasonicDistance(A4, A5);
+  delay(10);
+  float rightdist = 0.01723 * readUltrasonicDistance(A1, A0);
+  delay(10);
     if(rightdist > 100 or leftdist > 100)
     {
       if(leftdist > 100) L = 1;
     if(rightdist > 100) R = 1;
     }
     if(R == 1){
-    servo_9.write(60);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(8, LOW);
-    digitalWrite(4, LOW);
-    delay(2000);
-    servo_9.write(100);
+    Stop();
     delay(1000);
-    servo_9.write(80);  
-    digitalWrite(8, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(3, LOW);  
+    servo_9.write(70);
+    Move();
+    Backward();
+    delay(1500);
+    servo_9.write(110);
+    delay(1000);
+    servo_9.write(90);  
+    Forward();
     }
 
     if(L == 1){
-    servo_9.write(100);
-    digitalWrite(7, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(8, LOW);
-    digitalWrite(4, LOW);
-    delay(1500);
-    servo_9.write(60);
-    delay(500);
-    servo_9.write(80);  
-    digitalWrite(8, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(3, LOW);  
+    Stop();
+    delay(1000);
+    servo_9.write(110);
+    Move();
+    Backward();
+    delay(1000);
+    servo_9.write(70);
+    delay(1000);
+    servo_9.write(90);  
+    Forward();
     }
 
-    err = leftdist - rightdist;
-    U = err * Kp + Kd * (err - errold);
-    if(80+U > 107) servo_9.write(107);
-    else if(80+U < 55) servo_9.write(55);
-    else servo_9.write(80 + U);
-    errold = err;
-    delay(200);
-  
-  }
-  
-  // Left
-  while(L == 1){
-  leftdist = 0.01723 * readUltrasonicDistance(12, 13);
-  rightdist = 0.01723 * readUltrasonicDistance(11, 10);
-
- 
     err = leftdist - target;
+    U = err * Kp + Kd * (err - errold);
+    if(90+U > 110) servo_9.write(110);
+    else if(90+U < 70) servo_9.write(70);
+    else servo_9.write(90 + U);
+    errold = err;
+    delay(100);
+  }
+  
+                                                         
+
+    while(L == 1){
+
+  float leftdist = 0.01723 * readUltrasonicDistance(A4, A5);
+  delay(10);
+  float rightdist = 0.01723 * readUltrasonicDistance(A1, A0);
+  delay(10);
+
+    err = leftdist - targetL;
 
     U = err * Kp + Kd * (err - errold);
 
-    if(rightdist <= 10){
-      servo_9.write(105);
-      delay(500);
+
+        if (90 + U > 115) {
+      servo_9.write(115);
+  } else {
+     servo_9.write(90 + U);
   }
-
-  // if(frontdist <= 15){
-  //   digitalWrite(7, HIGH);
-  //   digitalWrite(3, HIGH);
-  //   digitalWrite(8, LOW);
-  //   digitalWrite(4, LOW);  
-  //     servo_9.write(55);
-  //     delay(1500);
-  //   digitalWrite(8, HIGH);
-  //   digitalWrite(4, HIGH);
-  //   digitalWrite(7, LOW);
-  //   digitalWrite(3, LOW);  
-  //     servo_9.write(105);
-  //     delay(500);
-  // }
-
-
-    if (80 + U > 107) {
-      servo_9.write(107);
-  } 
-   else {
-     servo_9.write(80 + U);
-  }
-
-errold = err;
-delay(200); 
-
+  errold = err;
+  delay(100);
 }
 
-  //Right
-  while(R == 1){
-  leftdist = 0.01723 * readUltrasonicDistance(12, 13);
-  rightdist = 0.01723 * readUltrasonicDistance(11, 10);
 
-    err = target - rightdist;
+
+
+    while(R == 1){
+    //Desicion
+  float leftdist = 0.01723 * readUltrasonicDistance(A4, A5);
+  float rightdist = 0.01723 * readUltrasonicDistance(A1, A0);
+
+    err = targetR - rightdist;
 
     U = err * Kp + Kd * (err - errold);
 
 
-  //     if(frontdist <= 15){
-  //   digitalWrite(7, HIGH);
-  //   digitalWrite(3, HIGH);
-  //   digitalWrite(8, LOW);
-  //   digitalWrite(4, LOW);  
-  //     servo_9.write(105);
-  //     delay(1500);
-  //   digitalWrite(8, HIGH);
-  //   digitalWrite(4, HIGH);
-  //   digitalWrite(7, LOW);
-  //   digitalWrite(3, LOW);  
-  //     servo_9.write(55);
-  //     delay(500);
-  // }
-
-
-    if (80 + U < 55) {
-      servo_9.write(55);
+        if (90 + U < 65) {
+      servo_9.write(65);
   } else {
-     servo_9.write(80 + U);
+     servo_9.write(90 + U);
   }
-  delay(200);
-errold = err;
+  errold = err;
+  delay(100);
 
+    if(leftdist <= 5){
+    servo_9.write(70);
+    delay(200);
+      }
+    }
   }
-}     
+
+void Forward() {
+  digitalWrite(7, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+}
+
+void Backward() {
+  digitalWrite(8, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+}
+
+void Stop() {
+  analogWrite(6, 0);
+  analogWrite(5, 0);
+}
+
+void Move() {
+  analogWrite(6, 84);
+  analogWrite(5, 77);
+}
+  
